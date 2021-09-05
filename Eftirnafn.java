b42e8ae2115e2fe1298aa0e931b5a15cd8a2bd10
desulol <87192248+desulol@users.noreply.github.com>
2021-09-05 01:31:51 +0000

    Add files via upload

---
+++ Eftirnafn.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+ *
+ * Tekur inn tvö arguments og prentar seinna fyrst
+ *
+ ******************************************************************************/
+
+public class Eftirnafn {
+
+    public static void main(String[] args) {
+        System.out.println((args[1]) + ", " + (args[0]));
+    }
+
+}
